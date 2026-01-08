9faa49139aff0375e88816e5e2e1711b7113fa65
jyk190121 <46877384+jyk190121@users.noreply.github.com>
2026-01-08 22:29:19 +0900

    커밋
    InputSystem 아직 이해 100% 아님
     - InputSystem 기반으로 GetKeyDown 동작 성공

--- Assets/1KJY/Scripts/Manager/StartManager.cs
+++ Assets/1KJY/Scripts/Manager/StartManager.cs
@@ -20,6 +20,7 @@ public class StartManager : MonoBehaviour
     public Button setCloseBtn;                          // 설정 창 닫기 버튼
     public Slider volumeSlider;
     public bool restart;
+    public Button infoBtn;
 
     public Texture2D cursorTexture;                     // 변경할 커서 이미지
     public Vector2 hotSpot = Vector2.zero;              // 클릭 위치 (좌상단이 0,0)
@@ -27,7 +28,7 @@ public class StartManager : MonoBehaviour
     public RectTransform selectionOutline;              // 유저에게 보여줄 테두리 이미지
     public Button backgroundCancelBtn;                  // 배경 클릭 감지용 버튼 추가
     //private int selectedBtnIndex = -1;                // 현재 선택된 스테이지 (-1은 선택 없음)
-    GameObject current;
+    GameObject currentObj;
 
     private void Awake()
     {
@@ -60,8 +61,9 @@ public class StartManager : MonoBehaviour
         endY.onClick.AddListener(EndGame);
         endN.onClick.AddListener(EnterGame);
 
-        settingBtn.onClick.AddListener(AudioImg);
+        settingBtn.onClick.AddListener(AudioSet);
         setCloseBtn.onClick.AddListener(EnterGame);
+        infoBtn.onClick.AddListener(ShowInfo);
 
         EndImg.gameObject.SetActive(false);
         volSetImg.gameObject.SetActive(false);
@@ -73,25 +75,6 @@ public class StartManager : MonoBehaviour
 
     private void Update()
     {
-
-        if (AudioManager.audioManager.IsPlaying("Battle"))
-        {
-            AudioManager.audioManager.StopBGM();
-            AudioManager.audioManager.PlayBGM("Intro");
-        }
-        if (Keyboard.current.escapeKey.wasPressedThisFrame == true)
-        {
-            GameEndYorN();
-        }
-
-        //현재 선택된 오브젝트를 가져옴
-        current = EventSystem.current.currentSelectedGameObject;
-
-        //if (Input.GetKeyDown(KeyCode.Return) && gameStartBtn != null ||
-        //    Input.GetKeyDown(KeyCode.Space) && gameStartBtn != null)
-        //{
-        //    GameStart();
-        //}
         //// 1. 키를 누르는 순간 (GetKeyDown과 동일)
         //if (Keyboard.current.spaceKey.wasPressedThisFrame)
         //{
@@ -110,130 +93,98 @@ public class StartManager : MonoBehaviour
         //    Debug.Log("스페이스바를 뗐습니다!");
         //}
 
-        //if (Keyboard.current.spaceKey.wasPressedThisFrame == true ||
-        //    Keyboard.current.enterKey.wasPressedThisFrame == true)
-        //{
-        //    GameStart();
-        //}
+        if (AudioManager.audioManager.IsPlaying("Battle"))
+        {
+            AudioManager.audioManager.StopBGM();
+            AudioManager.audioManager.PlayBGM("Intro");
+        }
+        if (Keyboard.current.escapeKey.wasPressedThisFrame == true)
+        {
+            GameEndYorN();
+            return;
+        }
+
+        //현재 선택된 오브젝트를 가져옴
+        currentObj = EventSystem.current.currentSelectedGameObject;
 
         Keyboard key = Keyboard.current;
         if (key == null) return;
 
+        //Enter는 EventSystem이 자동으로 Invoke해주기 때문에 처리가 필요없음
+        
+        //// 1. 엔터 키 입력 감지(현재 버튼 실행)
+        //if (Keyboard.current.enterKey.wasPressedThisFrame == true)
+        //{
+        //    // 2. 현재 이벤트 시스템에서 선택된(포커스된) 오브젝트 가져오기
+        //    GameObject currentSelected = currentObj;
+
+        //    if (currentSelected != null)
+        //    {
+        //        // 3. 해당 오브젝트에 버튼 컴포넌트가 있는지 확인
+        //        Button btn = currentSelected.GetComponent<Button>();
+        //        if (btn != null)
+        //        {
+        //            // 4. 버튼의 onClick 이벤트 실행
+        //            btn.onClick.Invoke();
+        //            Debug.Log($"{currentSelected.name} 실행");
+        //        }
+        //    }
+        //}
+        
+        //선택된 버튼 확인
+        UpdateSelectionOutline();
+
         // WASD와 화살표 입력을 하나로 합침
-        Vector2 moveInput = Vector2.zero;
+        //Vector2 moveInput = Vector2.zero;
 
-        if (key.wKey.wasPressedThisFrame || key.upArrowKey.wasPressedThisFrame) moveInput.y = 1;
-        else if (key.sKey.wasPressedThisFrame || key.downArrowKey.wasPressedThisFrame) moveInput.y = -1;
+        //if (key.wKey.wasPressedThisFrame || key.upArrowKey.wasPressedThisFrame) moveInput.y = 1;
+        //else if (key.sKey.wasPressedThisFrame || key.downArrowKey.wasPressedThisFrame) moveInput.y = -1;
 
-        if (key.aKey.wasPressedThisFrame || key.leftArrowKey.wasPressedThisFrame)
-        {
-            if (current == gameStartBtn.gameObject)
-            {
-                // EventSystem에 직접 명령을 내리는 것이 더 확실합니다.
-                EventSystem.current.SetSelectedGameObject(endBtn.gameObject);
-                return;
-            }
-            else
-            {
-                moveInput.x = -1;
-                return;
-            }
-        }
-        else if (key.dKey.wasPressedThisFrame || key.rightArrowKey.wasPressedThisFrame)
-        {
-            if (current == endBtn.gameObject)
-            {
-                EventSystem.current.SetSelectedGameObject(gameStartBtn.gameObject);
-                return;
-            }
-            else
-            {
-                moveInput.x = 1;
-                return;
-            }
-        }
-        // 진입하기 전 버튼 확인
-        SelectBtn(key);
-
-        // 1. 엔터 키 입력 감지(현재 버튼 실행)
-        if (Keyboard.current.enterKey.wasPressedThisFrame)
-        {
-            // 2. 현재 이벤트 시스템에서 선택된(포커스된) 오브젝트 가져오기
-            GameObject currentSelected = current;
-
-            if (currentSelected != null)
-            {
-                // 3. 해당 오브젝트에 버튼 컴포넌트가 있는지 확인
-                Button btn = currentSelected.GetComponent<Button>();
-                if (btn != null)
-                {
-                    // 4. 버튼의 onClick 이벤트 실행
-                    btn.onClick.Invoke();
-                    Debug.Log($"{currentSelected.name} 실행");
-                }
-            }
-        }
-       
-        //if (moveInput != Vector2.zero)
+        //// Update 함수 내의 입력 부분 수정
+        //if (key.aKey.wasPressedThisFrame == true || key.leftArrowKey.wasPressedThisFrame == true)
         //{
-        //    MoveCursor(moveInput);
+        //    // 왼쪽 끝(StartBtn)에서 왼쪽 입력 시 -> 오른쪽 끝(EndBtn)으로 이동
+        //    if (current == gameStartBtn.gameObject)
+        //    {
+        //        EventSystem.current.SetSelectedGameObject(endBtn.gameObject);
+        //        return; // 직접 이동시켰으므로 이후 로직 중단
+        //    }
         //}
-
-        //if (Input.GetKeyDown(KeyCode.Escape))
+        //else if (key.dKey.wasPressedThisFrame == true || key.rightArrowKey.wasPressedThisFrame == true)
         //{
-        //    GameEndYorN();
+        //    // 오른쪽 끝(EndBtn)에서 오른쪽 입력 시 -> 왼쪽 끝(StartBtn)으로 이동
+        //    if (current == endBtn.gameObject)
+        //    {
+        //        EventSystem.current.SetSelectedGameObject(gameStartBtn.gameObject);
+        //        return; // 직접 이동시켰으므로 이후 로직 중단
+        //    }
         //}
-
-
-
+        HandleLoopNavigation(key);
     }
 
-    public void GameStart()
+    void GameStart()
     {
-        //currentScene = SceneManager.GetActiveScene().buildIndex;
-        //SceneManager.LoadScene(currentScene + 1);
-        //SceneManager.LoadScene("BattleScene", LoadSceneMode.Additive);
-        //SceneManager.sceneLoaded += OnSceneLoaded;
-        //SceneManager.LoadScene("BattleScene", LoadSceneMode.Single);
         GameSceneManager.Instance.LoadSceneAsync("StageScene");
     }
 
-    //void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    //{
-    //    if (scene.name == "BattleScene")
-    //    {
-    //        if (GameManager.instance == null)
-    //        {
-    //            GameManager gameManager = FindFirstObjectByType<GameManager>();
-    //            if (gameManager != null)
-    //            {
-    //                GameManager.instance = gameManager;
-    //                DontDestroyOnLoad(gameManager.gameObject);
-    //                SceneManager.sceneLoaded -= OnSceneLoaded; // 다시 호출되지 않도록 구독 해제
-    //            }
-    //            else
-    //            {
-    //                Debug.LogError("GameManager가 존재하지 않습니다!");
-    //            }
-    //        }
-    //    }
-    //}
-
-    public void GameEndYorN()
+    void GameEndYorN()
     {
         volSetImg.gameObject.SetActive(false);
 
         if (!EndImg.gameObject.activeSelf)
         {
             EndImg.gameObject.SetActive(true);
+            endN.Select();
+            return;
         }
         else
         {
-            EndImg.gameObject.SetActive(false);
+            //EndImg.gameObject.SetActive(false);
+            EnterGame();
         }
     }
 
-    public void EndGame()
+    void EndGame()
     {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
@@ -246,16 +197,19 @@ public class StartManager : MonoBehaviour
     {
         EndImg.gameObject.SetActive(false);
         volSetImg.gameObject.SetActive(false);
-        gameStartBtn.Select();
+        //gameStartBtn.Select();
+        GameSceneManager.Instance.RestartScene();
     }
 
-    public void AudioImg()
+    public void AudioSet()
     {
         EndImg.gameObject.SetActive(false);
 
         if (!volSetImg.gameObject.activeSelf)
         {
             volSetImg.gameObject.SetActive(true);
+            setCloseBtn.Select();
+            return;
         }
         else
         {
@@ -274,45 +228,102 @@ public class StartManager : MonoBehaviour
         AudioManager.audioManager.bgmVolume = volumeSlider.value;
     }
 
-    //void MoveCursor(Vector2 direction)
+    //void SelectBtn(Keyboard key)
     //{
-    //    // 이동 로직 실행
-    //    Debug.Log($"입력 감지: {direction}");
+    //    if (key == null || currentObj == null) return;
+
+    //    if (currentObj != null)
+    //    {
+    //        // 선택된 오브젝트의 이름 출력
+    //        Debug.Log($"현재 선택된 UI: {currentObj.name}");
+
+    //        // 만약 버튼 컴포넌트가 있는지 확인하고 싶다면
+    //        if (currentObj.TryGetComponent<UnityEngine.UI.Button>(out var button))
+    //        {
+    //            // 여기서 버튼에 대한 추가 처리가 가능합니다.
+    //            // --- 유저용 UI 테두리 처리 ---
+    //            if (selectionOutline != null)
+    //            {
+    //                selectionOutline.gameObject.SetActive(true);
+
+    //                // 선택된 버튼의 위치로 테두리 이동
+    //                RectTransform btnRect = button.GetComponent<RectTransform>();
+    //                selectionOutline.position = btnRect.position;
+
+    //                // 테두리 크기를 버튼 크기에 맞춤
+    //                selectionOutline.sizeDelta = btnRect.sizeDelta;
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("현재 선택된 UI가 없습니다.");
+    //    }
+
     //}
 
-    void SelectBtn(Keyboard key)
+    void UpdateSelectionOutline()
     {
-        if (key == null || current == null) return;
-
-        if (current != null)
+        if (currentObj != null && currentObj.TryGetComponent<RectTransform>(out RectTransform targetRect))
         {
-            // 선택된 오브젝트의 이름 출력
-            Debug.Log($"현재 선택된 UI: {current.name}");
-
-            // 만약 버튼 컴포넌트가 있는지 확인하고 싶다면
-            if (current.TryGetComponent<UnityEngine.UI.Button>(out var button))
+            if (selectionOutline != null)
             {
-                // 여기서 버튼에 대한 추가 처리가 가능합니다.
-                // --- 유저용 UI 테두리 처리 ---
-                if (selectionOutline != null)
-                {
-                    selectionOutline.gameObject.SetActive(true);
-
-                    // 선택된 버튼의 위치로 테두리 이동
-                    RectTransform btnRect = button.GetComponent<RectTransform>();
-                    selectionOutline.position = btnRect.position;
-
-                    // 테두리 크기를 버튼 크기에 맞춤
-                    selectionOutline.sizeDelta = btnRect.sizeDelta;
-                }
+                selectionOutline.gameObject.SetActive(true);
+                selectionOutline.position = targetRect.position;
+                selectionOutline.sizeDelta = targetRect.sizeDelta;
             }
         }
-        else
-        {
-            Debug.Log("현재 선택된 UI가 없습니다.");
-        }
-
     }
 
+    void HandleLoopNavigation(Keyboard key)
+    {
+        //// A 또는 왼쪽 화살표: 시작 버튼에서 왼쪽 누르면 종료 버튼으로
+        //if (key.aKey.wasPressedThisFrame == true || key.leftArrowKey.wasPressedThisFrame == true)
+        //{
+        //    if (currentObj == gameStartBtn.gameObject)
+        //    {
+        //        EventSystem.current.SetSelectedGameObject(endBtn.gameObject);
+        //        return;
+        //    }
+        //}
+        //// D 또는 오른쪽 화살표: 종료 버튼에서 오른쪽 누르면 시작 버튼으로
+        //else if (key.dKey.wasPressedThisFrame == true || key.rightArrowKey.wasPressedThisFrame == true)
+        //{
+        //    if (currentObj == endBtn.gameObject)
+        //    {
+        //        EventSystem.current.SetSelectedGameObject(gameStartBtn.gameObject);
+        //        return;
+        //    }
+        //}
 
+        // 이동 로직 (Navigation이 None일 때만 정상 작동)
+        if (key.aKey.wasPressedThisFrame || key.leftArrowKey.wasPressedThisFrame)
+        {
+            if (currentObj == gameStartBtn.gameObject)
+                EventSystem.current.SetSelectedGameObject(endBtn.gameObject);
+            else if (currentObj == endBtn.gameObject)
+                EventSystem.current.SetSelectedGameObject(infoBtn.gameObject);
+            else if(currentObj == settingBtn.gameObject)
+                EventSystem.current.SetSelectedGameObject(gameStartBtn.gameObject);
+            else if (currentObj == infoBtn.gameObject)
+                EventSystem.current.SetSelectedGameObject(settingBtn.gameObject);
+        }
+        else if (key.dKey.wasPressedThisFrame || key.rightArrowKey.wasPressedThisFrame)
+        {
+            if (currentObj == endBtn.gameObject)
+                EventSystem.current.SetSelectedGameObject(gameStartBtn.gameObject);
+            else if (currentObj == gameStartBtn.gameObject)
+                EventSystem.current.SetSelectedGameObject(settingBtn.gameObject);
+            else if (currentObj == infoBtn.gameObject)
+                EventSystem.current.SetSelectedGameObject(endBtn.gameObject);
+            else if (currentObj == settingBtn.gameObject)
+                EventSystem.current.SetSelectedGameObject(infoBtn.gameObject);
+        }
+       
+    }
+
+    void ShowInfo()
+    {
+        print("아직 안만듬 인포창");
+    }
 }

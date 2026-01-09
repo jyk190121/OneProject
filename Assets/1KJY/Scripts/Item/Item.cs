4ee36cd237a68ae05d8088631f565ff81f197526
jyk190121 <46877384+jyk190121@users.noreply.github.com>
2026-01-09 18:01:11 +0900

    커밋
    골드 아이템 만들기

--- Assets/1KJY/Scripts/Item/Item.cs
+++ Assets/1KJY/Scripts/Item/Item.cs
@@ -17,14 +17,27 @@ public class Item : ScriptableObject
     public int ID => id;        //아이템 ID (오브젝트 생성 시 자동부여, 쓸일이 있을지는..)
     public string NAME;         //아이템 이름
     public Sprite IMAGE;        //아이템 이미지
+
+    [Header("업그레이드 상점")]
     public int PRICE;           //아이탬 가격 (업그레이드 상점용)
-    public int ENHANCE;         //강화 횟수(최대 3강)
-    public int ATK;             //물리공격력 증가 스텟
-    public int MATK;            //마법공격력 증가 스텟
+    public int ENHANCE;         //강화 횟수(최대 3강) 임의로
+    public int GOLD;            //골드 (아이템으로 얻게되는 화폐)
+
+    [Header("아이템 스텟")]
+    public int MINATK;          //최소 물리공격력 스텟
+    public int MINMATK;         //최소 마법공격력 스텟
+    public int ATK;             //(최대)물리공격력 스텟
+    public int MATK;            //(최대)마법공격력 스텟
+    public int PLUSATK;         //물리공격력 증가 스텟
+    public int PLUSMATK;        //마법공격력 증가 스텟
     public float HP;            //체력회복 스텟
     public float SHILD;         //방어도회복 스텟
     public float POISON;        //독 누적
     //public int COUNT;           //치명타, 메가치명타
+
+    [Header("기절확률")]
+    [Range(0, 1)]
+    public float STUNED;
 
     [Header("아이템 설명")]
     [TextArea(minLines: 1, maxLines: 10)]
